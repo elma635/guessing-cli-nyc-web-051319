49d76113558b0ab9a2ffe4b6282f93a0294a7ec0
Elma Rahman <elmarahman635@gmail.com>
2019-05-07 21:29:40 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,22 +4,23 @@ def guessing_cli
   user_input = " " #dont need this bc using get in th
       while user_input
         user_input = gets.strip
-        rand_num = rand 1..6 #returning 1 single number from the range 
+        rand_num = rand 1..6 #returning 1 single number from the range
             if user_input == rand_num
               puts "You guessed the correct number!"
 
     "the number has been guessed incorrectly"
 
-when "exit "
-    puts "Goodbye!" #allow user to exit the program
-  break
+  elsif user_input == "exit"
+
+                puts "Goodbye!" #allow user to exit the program
+                    break
 
       end
     end
   end
 end
 
-
+#use when , when you're using a case statement
 #takes in input from command line
 #Comparing that input to a random number that has been generated
 #Printing out a statement You guessed the correct number!
