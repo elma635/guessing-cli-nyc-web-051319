9382329e3bbbaec415255604099c8afbe8cabc3a
Elma Rahman <elmarahman635@gmail.com>
2019-05-07 20:32:29 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,11 @@
 # Code your solution here!
+
+def guessing_cli 
+
+
+#takes in input from command line
+#Comparing that input to a random number that has been generated
+#Printing out a statement You guessed the correct number!
+#if the number has been guessed correctly, or The computer guessed <number>.
+# if the number has been guessed incorrectly ---
+#Allowing the user to exit the program when exit is the input
