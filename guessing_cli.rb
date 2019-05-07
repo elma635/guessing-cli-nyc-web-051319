2b266c072e526bd3dc05ce3566c90bc5207340c7
Elma Rahman <elmarahman635@gmail.com>
2019-05-07 20:35:49 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,12 @@
 
 def guessing_cli 
 
+user_input = " "
+
+if user_input.to_i.between?(1,6)
+
+
+end
 
 #takes in input from command line
 #Comparing that input to a random number that has been generated
@@ -9,3 +15,4 @@ def guessing_cli
 #if the number has been guessed correctly, or The computer guessed <number>.
 # if the number has been guessed incorrectly ---
 #Allowing the user to exit the program when exit is the input
+
